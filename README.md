0e70097e22f4d25a3b05beb671a21427c96d6813
ging-dev <thanh1101dev@gmail.com>
2022-04-01 22:41:02 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sprite-splitter
+# Sprite splitter
+
+## Requirements
+Linux with `imagemagick` installed.
+
+## Usage
+
+```bash
+$ ./sprite-splitter -i input.png -o output
+```
+
+## What is it?
+This was created during my learning to make games, it is used to split the sprites of a transparent spritesheet. See example below:
+
+![](https://imgur.com/EuhoZpi.png)
+![](https://imgur.com/F7lj5vg.png)
---
+++ sprite-splitter.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+FILENAME=$1
+OUTPUT_DIR=$2
+
+convert $FILENAME -alpha extract tmp_mask.png 2> /dev/null
+
+OLD_IFS=$IFS
+IFS=$'\n'
+
+# https://imagemagick.org/script/connected-components.php
+arr=(`convert tmp_mask.png \
+    -define connected-components:verbose=true \
+    -define connected-components:area-threshold=10 \
+    -define connected-components:mean-color=true \
+    -connected-components 8 \
+    null: | tail -n +2 | grep --color=never 'gray(255)'`)
+
+IFS=$OLD_IFS
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+fi
+
+num=${#arr[*]}
+for ((i=0; i<num; i++))
+do
+    rect=`echo "${arr[$i]}" | awk '{print $2}'`
+    convert $FILENAME -crop $rect +repage -background none ${OUTPUT_DIR}/${i}.png
+    echo -n '.'
+done
+
+rm tmp_mask.png
